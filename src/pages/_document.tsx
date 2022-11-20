e5b579d4416055f21d97bece206a386d13bd2037
Bruno Andrade <brunoandrade99@gmail.com>
2022-11-20 15:48:24 -0300

    feat: update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -37,7 +37,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
 
           <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet" />
-          <link rel="icon" href="/logo.png" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
