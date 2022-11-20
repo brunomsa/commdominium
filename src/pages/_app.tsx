9ab01a262e976c3537fc25429bde7a294fd7ecf9
Bruno Andrade <brunoandrade99@gmail.com>
2022-11-20 17:02:33 -0300

    comment loading page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,20 +22,20 @@ import theme from '../styles/theme';
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const handleStart = (url: string) => url !== Router.asPath && setLoading(true);
-    const handleComplete = (url: string) => url === Router.asPath && setLoading(false);
+  // useEffect(() => {
+  //   const handleStart = (url: string) => url !== Router.asPath && setLoading(true);
+  //   const handleComplete = (url: string) => url === Router.asPath && setLoading(false);
 
-    Router.events.on('routeChangeStart', handleStart);
-    Router.events.on('routeChangeComplete', handleComplete);
-    Router.events.on('routeChangeError', handleComplete);
+  //   Router.events.on('routeChangeStart', handleStart);
+  //   Router.events.on('routeChangeComplete', handleComplete);
+  //   Router.events.on('routeChangeError', handleComplete);
 
-    return () => {
-      Router.events.off('routeChangeStart', handleStart);
-      Router.events.off('routeChangeComplete', handleComplete);
-      Router.events.off('routeChangeError', handleComplete);
-    };
-  });
+  //   return () => {
+  //     Router.events.off('routeChangeStart', handleStart);
+  //     Router.events.off('routeChangeComplete', handleComplete);
+  //     Router.events.off('routeChangeError', handleComplete);
+  //   };
+  // });
 
   return (
     <>
