867347e42c127fbbb6abfaa6c3d2ae1dfbaf4ea4
Bruno Andrade <brunoandrade99@gmail.com>
2022-11-20 22:30:40 -0300

    styles: set margin bottom to home cards

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -11,12 +11,20 @@ export const Home = styled.div`
     display: flex;
     flex-grow: 1;
     gap: 40px;
+
+    > div {
+      margin-bottom: 32px;
+    }
   }
 
   @media screen and (max-width: 1080px) {
     .cards {
       flex-direction: column;
       gap: 20px;
+
+      > div:first-child {
+        margin-bottom: 0;
+      }
     }
   }
 `;
